0c4ea64c9af32262325926e58530fe8bcd63771c
sami.khan <sami.khan@tylertech.com>
2019-06-28 09:52:02 -0500

    Initial Commit

---
+++ cache/cache.go
@@ -0,0 +1,88 @@
+package cache
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"os/user"
+	"path"
+	"time"
+)
+
+const (
+	zipPath = "/tldr.zip"
+)
+
+type Cache struct {
+	location string
+	remote   string
+	ttl      time.Duration
+}
+
+func Create(remote string, ttl time.Duration) (*Cache, error) {
+	dir, err := GetCacheDir()
+	if err != nil {
+		return nil, fmt.Errorf("ERROR: getting cache directory: %s", err)
+	}
+
+	cache := &Cache{location: dir, remote: remote, ttl: ttl}
+
+	info, err := os.Stat(dir)
+	if os.IsNotExist(err) {
+		err = cache.CreateCacheFolder()
+		if err != nil {
+			return nil, fmt.Errorf("ERROR: creating cache directory: %s", err)
+		}
+		err = cache.LoadFromRemote()
+		if err != nil {
+			return nil, fmt.Errorf("ERROR: loading data from remote: %s", err)
+		}
+	} else if err != nil || info.ModTime().Before(time.Now().Add(-ttl)) {
+		// err = cache.Reload()
+		// if err != nil {
+		// 	return nil, fmt.Errorf("ERROR: reloading cache: %s", err)
+		// }
+	}
+
+	return cache, nil
+}
+
+func (cache *Cache) CreateCacheFolder() error {
+	return os.MkdirAll(cache.location, 0755)
+}
+
+func (cache *Cache) LoadFromRemote() error {
+	dir, err := os.Create(cache.location + zipPath)
+	if err != nil {
+		return fmt.Errorf("ERROR: creating cache: %s", err)
+	}
+	defer dir.Close()
+
+	resp, err := http.Get(cache.remote)
+	if err != nil {
+		return fmt.Errorf("ERROR: downloading tldr.zip: %s", err)
+	}
+	defer resp.Body.Close()
+
+	_, err = io.Copy(dir, resp.Body)
+	if err != nil {
+		return fmt.Errorf("ERROR: saving tldr.zip to cache: %s", err)
+	}
+
+	//TODO: Unzip tldr.zip
+	Unzip(dir.Name+"/tldr.zip", dir)
+
+	return nil
+}
+
+func GetCacheDir() (string, error) {
+	usr, err := user.Current()
+	if err != nil {
+		return "", fmt.Errorf("ERROR: getting current user: %s", err)
+	}
+	if usr.HomeDir == "" {
+		return "", fmt.Errorf("ERROR: loading current user's home directory")
+	}
+	return path.Join(usr.HomeDir, ".tldr"), nil
+}
---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"tldr/cache"
+
+	"gopkg.in/alecthomas/kingpin.v2"
+)
+
+const (
+	remoteURL = "http://tldr-pages.github.io/assets/tldr.zip"
+	ttl       = time.Hour * 24 * 7
+)
+
+var (
+	clear  = kingpin.Flag("clear-cache", fmt.Sprintf("Clear local cache then update from %s", remoteURL)).Short('c').Bool()
+	update = kingpin.Flag("update", fmt.Sprintf("Clear local cache then update from %s", remoteURL)).Short('u').Bool()
+	random = kingpin.Flag("raundom", "Random page for testing purposes").Short('r').Default("false").Bool()
+	page   = kingpin.Arg("command", "Name of the command. (e.g. tldr tar)").Strings()
+)
+
+func main() {
+	kingpin.UsageTemplate(kingpin.CompactUsageTemplate).Version("1.0").Author("Sami Khan")
+	kingpin.Parse()
+	cache.Create(remoteURL, ttl)
+}
